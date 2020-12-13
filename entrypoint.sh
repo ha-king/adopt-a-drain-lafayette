969980d91f88483dee4000686e052df7f3948474
ha-king <003king@gmail.com>
2020-12-13 09:19:29 -0600

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-
 bundle exec rake db:setup
-
 bundle exec rake db:seed
-
-# Remove a potentially pre-existing server.pid for Rails.
 rm -f tmp/pids/server.pid
-
-# Run the Rails server
-bundle exec rails server -b 0.0.0.0 -p 80
+bundle exec rails server -p 80 -b '0.0.0.0'
