0a26980a05032ad11a0eb1b6d6b4d385d2590e95
ha-king <003king@gmail.com>
2021-02-20 06:16:49 -0600

    fooling w/ environments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,48 +1,72 @@
 # frozen_string_literal: true
 
+options = Rack::Server.new.options
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
 
-  # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
+  # asset host
+  #config.action_controller.asset_host = "http://#{options[:Host]}:#{options[:Port]}"
+  config.action_controller.asset_host = "https://adopt-a-drain.infascination.com"
+  config.action_mailer.asset_host = config.action_controller.asset_host
+
+  # Do not eager load code on boot.
   config.eager_load = false
-
-  # Configure static file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = {'Cache-Control' => 'public, max-age=3600'}
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {host: 'infascination.com'}
 
-  # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
 
-  # Randomize the order test cases are executed.
-  config.active_support.test_order = :random
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
 
-  # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Run jobs inline to test effects
-  config.active_job.queue_adapter = :inline
+  # For Mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {address: 'adopt-a-drain.infascination.com', port: 25}
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '25',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: ENV['SENDGRID_DOMAIN'],
+  }
 end
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '25',
+  authentication: :plain,
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: ENV['SENDGRID_DOMAIN'],
+}
