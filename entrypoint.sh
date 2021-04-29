a288c698267f090d8469aae3561a4a29c28af8aa
ha-king <003king@gmail.com>
2021-04-29 14:44:11 -0500

    updating entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 bundle exec rake db:setup
-bundle exec rake data:load_drains
+#bundle exec rake data:load_drains
 rm -f tmp/pids/server.pid
 RAILS_ENV=$RAILS_ENV bundle exec rails server -p 80 -b '0.0.0.0'
