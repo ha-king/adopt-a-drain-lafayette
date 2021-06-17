c3f8c7041079bf30469b1fb92969d04d3ab5e34b
David <dpradonline@gmail.com>
2021-06-17 10:05:44 -0700

    updated sendgrind info

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,13 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: '25',
   authentication: :plain,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  user_name: 'apikey',
+  password: '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',
   domain: 'infascination.com',
   enable_starttls_auto: true,
 }
+
+# this is just a test api key, it is not real
 
 # Initialize the Rails application.
 Rails.application.initialize!
